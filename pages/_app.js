1c6eecd4da62cf5577313d02f2bf0ae09af05c4a
Swimrad <Ryan@uwatermarketing.com>
2025-09-04 18:25:04 +0000

    feat(header): announcement bar + deep nav + dual CTAs + 56px logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-// pages/_app.js
 import "@/styles/globals.css";
+import AnnouncementBar from "@/components/AnnouncementBar";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <AnnouncementBar />
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
